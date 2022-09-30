92b995766ac5302a4b802d6c2a2c482ba4332a61
emreceva <86777959+emreceva@users.noreply.github.com>
2022-09-30 11:48:39 +0300

    Initial commit

---
+++ src/main/java/com/ceva/employeeregistrationproject/bootstrap/DataGenerator.java
@@ -0,0 +1,67 @@
+package com.ceva.employeeregistrationproject.bootstrap;
+
+public class DataGenerator {
+    private static List<Employee> employeeList = new ArrayList<>();
+
+    public static void saveEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public static List<Employee> readAllEmployees() {
+        return employeeList;
+    }
+
+    public static List<String> getAllStates() {
+        return Arrays.asList(
+                "Alaska",
+                "Alabama",
+                "Arkansas",
+                "Arizona",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Iowa",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Massachusetts",
+                "Maryland",
+                "Maine",
+                "Michigan",
+                "Minnesota",
+                "Missouri",
+                "Mississippi",
+                "Montana",
+                "NorthCarolina",
+                "NorthDakota",
+                "Nebraska",
+                "NewHampshire",
+                "NewJersey",
+                "NewMexico",
+                "Nevada",
+                "NewYork",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "RhodeIsland",
+                "SouthCarolina",
+                "SouthDakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Virginia",
+                "Vermont",
+                "Washington",
+                "Wisconsin",
+                "WestVirginia",
+                "Wyoming");
+    }
+}
